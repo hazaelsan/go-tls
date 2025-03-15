d4df763ce898c3af814f13e849b97fbac7896827
Hazael Sanchez <hazaelsan@gmail.com>
2025-03-15 09:22:50 +0000

    Rename dummy empty package

--- proto/v1/tls/empty.go
+++
@@ -1,2 +0,0 @@
-// Package tls is a dummy package to get `go mod tidy` to work correctly.
-package tls
---
+++ proto/v1/tlspb/empty.go
@@ -0,0 +1,2 @@
+// Package tlsb is a dummy package to get `go mod tidy` to work correctly.
+package tlspb
